c044958e03dbcfdae77328d8b1a89c7cd9307b82
Diego Castro <8404422+dfdcastro@users.noreply.github.com>
2025-06-10 16:12:33 -0300

    Creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Diego on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IntroducingSwiftUI
 Introducing SwiftUI with Apple Tutorial
+
+Example project using https://developer.apple.com/tutorials/swiftui
