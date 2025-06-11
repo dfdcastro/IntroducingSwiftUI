263b7ccb6f50a975aa1400a03f5714d737119b07
Diego Castro <8404422+dfdcastro@users.noreply.github.com>
2025-06-11 08:54:26 -0300

    Building lists and navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Diego on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
